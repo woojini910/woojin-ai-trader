06fa1de96f8c319671c64efb8768f1147a83a6c6
woojini910 <woojini910@gmail.com>
2026-04-30 23:36:55 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+function App() {
+  return <h1>우진 AI 트레이더 정상 작동 🚀</h1>
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
